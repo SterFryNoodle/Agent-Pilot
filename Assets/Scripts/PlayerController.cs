bb1f6f239a201788a15346438c28fae1a499f69b
SterFryNoodle <revangcruz@yahoo.com>
2024-02-13 19:42:37 -0800

    Added in new input system pack; Put terrain file into Scene folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalRotate =  Input.GetAxis("Horizontal");
-        float verticalRotate = Input.GetAxis("Vertical");
+        float horizontalRotate = movement.ReadValue<Vector2>().x; //get value from player input through new
+        float verticalRotate = movement.ReadValue<Vector2>().y; //input system.
+
+        //float horizontalRotate =  Input.GetAxis("Horizontal");
+        //float verticalRotate = Input.GetAxis("Vertical");
+        Debug.Log(horizontalRotate);
+        Debug.Log(verticalRotate);
     }
 }
