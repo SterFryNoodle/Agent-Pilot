ba32b133be5a4421bf9a75dc9a9e0f9b1b8244ed
SterFryNoodle <revangcruz@yahoo.com>
2024-02-14 23:16:04 -0800

    Organized code into a function; Added code for rotation movement of player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,11 +30,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalRotate = movement.ReadValue<Vector2>().x; //get value from player input through new
-        float verticalRotate = movement.ReadValue<Vector2>().y; //input system.
+        MovePlayer();
+        RotatePlayer();
+    }
 
-        float xOffset = horizontalRotate * Time.deltaTime * controlSpeed; //offset value from player original position
-        float yOffset = verticalRotate * Time.deltaTime * controlSpeed;
+    void MovePlayer()
+    {
+        float horizontalMove = movement.ReadValue<Vector2>().x; //get value from player input through new
+        float verticalMove = movement.ReadValue<Vector2>().y; //input system.
+
+        float xOffset = horizontalMove * Time.deltaTime * controlSpeed; //offset value from player original position
+        float yOffset = verticalMove * Time.deltaTime * controlSpeed;
 
         float newXPos = transform.localPosition.x + xOffset;//stores new position value
         float newYPos = transform.localPosition.y + yOffset;
@@ -43,5 +49,13 @@ public class PlayerController : MonoBehaviour
         float clampYPos = Mathf.Clamp(newYPos, -maxYRange, maxYRange);
 
         transform.localPosition = new Vector3(clampXPos, clampYPos, transform.localPosition.z); //allows to move object in local vector3 axis.
+    }
+
+    void RotatePlayer()
+    {
+        
+
+
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0);
     }
 }
