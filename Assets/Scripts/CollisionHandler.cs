5c40f27dd30726f4089e5143d0f4e4938ed29e8f
SterFryNoodle <revangcruz@yahoo.com>
2024-02-26 18:33:58 -0800

    Added tags for colliding objects; added collider script to player

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+    void PlayerCollision(Collision collision)
+    {
+        if (collision.gameObject.tag == "EnemyTag")
+        {
+            Debug.Log("Player Ship triggered Enemy collider");
+
+        }
+        else if (collision.gameObject.tag == "TerrainTag")
+        {
+            Debug.Log("Player Ship collided with Bridge");
+        }
+    }
 }
