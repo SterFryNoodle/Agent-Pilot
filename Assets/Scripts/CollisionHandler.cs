ab0fd970a65bda341360f263cf951ca19518bf22
SterFryNoodle <revangcruz@yahoo.com>
2024-02-29 22:12:58 -0800

    Added explosion trigger on collision; fixed player starting pt

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,17 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] float delayTime = 1f;   
-    
+    [SerializeField] float delayTime = 1f;
+    [SerializeField] ParticleSystem explosiveFX;
+
+    bool isStartingSequence;
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(name + " Triggered with " + other);
+
+        if (isStartingSequence)
+        {
+            return;
+        }
         
         StartCrashSequence();
     }
 
     void StartCrashSequence()
     {        
+        isStartingSequence = true;
+        explosiveFX.Play();
         GetComponent<PlayerController>().enabled = false; //Disables the PlayerController script
         Invoke("LoadLevel", delayTime); //Reloads the level after "delayTime" amount of time
     }
