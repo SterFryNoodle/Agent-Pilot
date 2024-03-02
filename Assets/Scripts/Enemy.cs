e607444fedb9bc821027d4b612d552b1d0abf4dd
SterFryNoodle <revangcruz@yahoo.com>
2024-03-01 22:54:18 -0800

    Create a parent to instantiated explosions in enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform effectsSpawner;
 
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(deathFX, transform.position, Quaternion.identity); //Instanstiates FX on position of enemy object w/ no rotation
+        GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity); //Instanstiates FX on position of enemy object w/ no rotation
+        vfx.transform.parent = effectsSpawner; //setting the gameobject attached to effectsSpawner to be the parent of gameobject attached to vfx.
         Destroy(gameObject);
     }
 }
