5c0186b8d8230e772cb4a5ac96a30423d753517b
SterFryNoodle <revangcruz@yahoo.com>
2024-03-05 22:43:24 -0800

    Set all instantiated vfx to be parented within the Runtime Spawner through use of tags

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
-    [SerializeField] GameObject damageFX;
-    [SerializeField] Transform effectsSpawner;    
+    [SerializeField] GameObject damageFX;       
     [SerializeField] int increasePoints = 5;
     [SerializeField] int hitPoints = 2;    
 
     ScoreBoard scoreBoard; //declares variable of Scoreboard type, allowing to communicate w/ the other class.
+    GameObject effectsSpawner; 
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>(); //Finds the first variable of Scoreboard type upon start, which works in this instance b/c there is only one variable of that type.
+        effectsSpawner = GameObject.FindWithTag("RuntimeSpawner"); //Setting effectsSpawner as a GameObject of what holds "RuntimeSpawner" tag
         AddRigidBody();
     }
 
@@ -46,12 +46,13 @@ public class Enemy : MonoBehaviour
     void InstantiateEnemyExplosionVFX()
     {
         GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity); //Instanstiates FX on position of enemy object w/ no rotation
-        vfx.transform.parent = effectsSpawner; //setting the gameobject attached to effectsSpawner to be the parent of gameobject attached to vfx.
+        vfx.transform.parent = effectsSpawner.transform; //setting the transform of what tag effectsSpawner holds to be the parent of
+                                                         //the GameObject that vfx is instantiating
     }
 
     void DamageEnemyVFX()
     {
         GameObject hitVFX = Instantiate(damageFX, transform.position, Quaternion .identity);
-        hitVFX.transform.parent = effectsSpawner;
+        hitVFX.transform.parent = effectsSpawner.transform;
     }
 }
